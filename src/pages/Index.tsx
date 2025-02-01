154df4adef21df79f47e44fe8b9504d40b290375
pitchsidevhs <pitchside.vhs@gmail.com>
2025-02-01 13:35:30 +0530

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,13 @@
+import { HeroSection } from "@/components/hero-section";
+import { FeaturesSection } from "@/components/features-section";
+
+const Index = () => {
+  return (
+    <main className="min-h-screen">
+      <HeroSection />
+      <FeaturesSection />
+    </main>
+  );
+};
+
+export default Index;
